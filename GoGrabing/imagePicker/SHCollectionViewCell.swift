510aef97c334cf961346fef077201d9b3cbaee96
Darshan <darshan@Darshans-MacBook-Pro.local>
2020-04-17 11:07:24 -0400

    Image Picker Assentials added

---
+++ GoGrabing/imagePicker/SHCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  Created by Darshan Patel on 2020-03-22.
+//  Copyright © 2020 GoGrabing. All rights reserved.
+//
+
+
+import UIKit
+
+class SHCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var filterNameLabel: UILabel!
+    @IBOutlet var imageView: UIImageView!
+}
